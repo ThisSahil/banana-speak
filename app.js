f3e1642035efebf2e002e2ed1d7f4a30022f66c1
Sahil Chhabra <lastspin290901@gmail.com>
2022-10-28 00:45:31 +0530

    taking input from text-area

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTransLate = document.querySelector("#translate-btn");
+
+var textArea = document.querySelector("#text-area");
+
+btnTransLate.addEventListener("click", function clickEventListner() {
+  console.log(textArea.value);
+});
