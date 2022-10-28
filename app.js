f42cc0e9ea7f2874d2233fb12faf2873f2c71def
Sahil Chhabra <lastspin290901@gmail.com>
2022-10-28 05:36:31 +0530

    styling and JS done

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ var textArea = document.querySelector("#text-area");
 
 var outputBox = document.querySelector("#output-box");
 
-outputBox.innerHTML = "Hello Jii";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-btnTransLate.addEventListener("click", function clickEventListner() {
-  outputBox.innerHTML = "ajdfjsdvn " + textArea.value;
-});
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured ", error);
+}
+
+function clickEventListner() {
+  var inputText = textArea.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputBox.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+btnTransLate.addEventListener("click", clickEventListner);
